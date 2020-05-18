106f1155cd05ddc1270c584d54ea6e9083c29cd5
Christopher Lewis <clew1996@gmail.com>
2020-05-17 20:01:01 -0700

    more comments

--- LewisLogging/Logger.h
+++ LewisLogging/Logger.h
@@ -55,7 +55,7 @@ class Logger
     public:
 /*******************************************************
 *       Non-parameterized constructor, will set the 
-*           severity level to 'OFF' & FILE pointer to null.
+*           severity level to 'OFF' & FILE pointer to nullptr.
 *
 *******************************************************/
 
@@ -64,8 +64,8 @@ class Logger
 /*******************************************************
 *
 *       Parameterized constructor, will take a given filename
-*           open a file of that name and assign it to the
-*           FILE pointer to be used as the output stream.
+*           open a text file of that name in 'WRITE_MODE' 
+            and assign it to the FILE pointer to be used as the output stream.
 *
 *
 *******************************************************/
@@ -81,18 +81,39 @@ class Logger
 
 /*******************************************************
 *
-*       
-*
-*
+*       Will take a given severity, passed to it by
+*           the caller and change the logging level to the 
+*           passed severity.
+*           This will determine what level of log messages
+*           are displayed by caller.
 *
 *******************************************************/
 
         void setLevel(Severity severity);
 
 /*******************************************************
+*       Displays log messages with the 'message' that is passed
+*           by the caller along with a timestamp for when
+*           the log message occurred.
+*       
+*       Output Format To Expect:
+*           DAY MON DATE HOUR:MINUTE:SECOND YEAR   
+*                   "This is a {LogLevel} message!"
 *
-*   
+*       The 'if' statments in each of the logging level's
+*           are used to determine if that level's message should be displayed
+*           or not, due to enumerated datatypes being represented
+*           as integers starting from {0,1,2...etc}. 
+*           This allows the logic to ensure that only the current
+*           log levels & all levels 'higher' will be displayed.  
 *
+*       Example: 
+*           If the logging level is set to WARN, & the caller
+*               calls the 'warn' function; the logger will display the
+*               warn,error & fatal messages.
+*           However if the logging level is set to WARN & the caller
+*               calls the 'trace' function, the trace log message will not be 
+*               displayed.           
 *
 *******************************************************/
         void trace(const char* message);
