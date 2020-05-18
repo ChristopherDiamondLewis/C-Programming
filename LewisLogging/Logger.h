d993258b2aec4a994fa902dc6d8a7a0752a749e8
Christopher Lewis <clew1996@gmail.com>
2020-05-17 19:27:29 -0700

    More comments

--- LewisLogging/Logger.h
+++ LewisLogging/Logger.h
@@ -27,22 +27,84 @@ class Logger
 {
 
     private:
+/*****************************************************
+*       Enumurated datatype to represent the severity
+*           of the Logger, works with the 'setLevel'
+*           function to determine which logging level
+*           messages are displayed.
+*
+******************************************************/
+
         Severity priv_severityLevel;
-        FILE *priv_FilePtr = nullptr;
+
+/*****************************************************
+*   
+*       File pointer used to determine which output stream
+*           the log messages will be written to.
+*       A FILE pointer can point to stderr or an
+*       open file depending on how the caller declares 
+*       the logger object. 
+*
+******************************************************/
         
+        FILE *priv_FilePtr;
+
+/******************************************************/
+/******************************************************/       
 
     public:
+/*******************************************************
+*       Non-parameterized constructor, will set the 
+*           severity level to 'OFF' & FILE pointer to null.
+*
+*******************************************************/
+
         Logger();
+
+/*******************************************************
+*
+*       Parameterized constructor, will take a given filename
+*           open a file of that name and assign it to the
+*           FILE pointer to be used as the output stream.
+*
+*
+*******************************************************/
+
         Logger(const char* fileName);
+
+/*******************************************************
+*
+*
+*******************************************************/
+
         virtual ~Logger();
 
+/*******************************************************
+*
+*       
+*
+*
+*
+*******************************************************/
+
         void setLevel(Severity severity);
+
+/*******************************************************
+*
+*   
+*
+*
+*******************************************************/
         void trace(const char* message);
         void debug(const char* message);
         void info (const char* message);
         void warn (const char* message);
         void error(const char* message);
         void fatal(const char* message);
+/*******************************************************
+*
+*
+*******************************************************/
 
         const char* severitys[NUM_SEVERITIES] = {"TRACE", "DEBUG" , "INFO", "WARN", "ERROR", "FATAL", "OFF"};
 
