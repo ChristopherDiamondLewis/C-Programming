779368cb26ef4d678994d2850ecd850a25dc20c7
Christopher Lewis <clew1996@gmail.com>
2020-05-30 13:42:22 -0700

    added more format changes to .c

--- LinkedListFun/LinkedList.c
+++ LinkedListFun/LinkedList.c
@@ -2,160 +2,139 @@
 
 #include "LinkedList.h"
 
-
-
-
-
 node_t *makeNode(int value)
-{	
-	node_t *newNode = (node_t*)malloc(sizeof(node_t));
+{
+	node_t *newNode = (node_t *)malloc(sizeof(node_t));
 
-	if ( newNode ==  NULL )
+	if (newNode == NULL)
 	{
 		printf("Error: not enough memory!\n");
 		return -1;
 	}
-	
+
 	newNode->value = value;
-	newNode->next  = NULL;
-	
+	newNode->next = NULL;
+
 	return newNode;
 }
 node_t *removeFront(node_t *head)
 {
 	node_t *newHead = head;
-	
-	
+
 	newHead = newHead->next;
 	head = NULL;
-	
+
 	free(head);
 
 	return newHead;
-	
-	
 }
 node_t *reverse(node_t *head)
 {
-	
+
 	node_t *next;
-	node_t *prev =  NULL;
+	node_t *prev = NULL;
 	node_t *current = head;
-	
+
 	// traversal step...
-	while ( current != NULL )
+	while (current != NULL)
 	{
 		next = current->next;
-		current->next = prev;  // linked backwards to prev node...
+		current->next = prev; // linked backwards to prev node...
 		prev = current;
 		current = next;
 	}
-	
-	
+
 	head = prev;
-	
-	return head;	
-	
+
+	return head;
 }
-	
-void addToIndex(node_t *head, int index , int nodeValue)
+
+void addToIndex(node_t *head, int index, int nodeValue)
 {
-	
+
 	int counter = 0;
-	
+
 	node_t *newNode = makeNode(nodeValue);
-	
-	
+
 	node_t *currNode = head;
 	node_t *prevNode;
-	
-	
-	while(currNode->next != NULL && counter < index )
+
+	while (currNode->next != NULL && counter < index)
 	{
 		prevNode = currNode;
 		currNode = currNode->next;
 		counter++;
 	}
-	
+
 	prevNode->next = newNode;
-	newNode->next  = currNode;
-	
-	
+	newNode->next = currNode;
 }
 
 void deleteFromIndex(node_t *head, int index)
 {
 	int counter = 0;
-	
-	
+
 	// need two traversals to keep track of previous node and current node...
 	node_t *current = head;
 	node_t *prev = current;
-	
+
 	while (current->next != NULL && counter < index)
 	{
-		
+
 		prev = current;
 		current = current->next;
 		counter++;
 	}
-	
+
 	prev->next = current->next;
-	
+
 	free(current);
-	
-	
 }
 void appendList(node_t **head, int value)
 {
-	
-	node_t *newNode   =  makeNode(value);
+
+	node_t *newNode = makeNode(value);
 	node_t *traversal = *head;
-	
-	
-	if( *head == NULL )
-	{	
+
+	if (*head == NULL)
+	{
 		*head = newNode;
 		return;
 	}
-	
-	
+
 	// Traverse to the end of the list if it is bigger than 1...
-	while ( traversal->next != NULL ) 
+	while (traversal->next != NULL)
 	{
 		traversal = traversal->next;
 	}
-	
+
 	// once we reach the end we will link the new node to the end of list...
 	traversal->next = newNode;
 }
 bool foundInList(node_t *head, int value)
 {
 	bool found = false;
-	
+
 	node_t *traversal = head;
-	
-	while(traversal->next != NULL)
+
+	while (traversal->next != NULL)
 	{
-		if(traversal->value == value)
+		if (traversal->value == value)
 		{
 			found = true;
 		}
-		
+
 		traversal = traversal->next;
 	}
-	
-	
-	return found;	
-	
-	
+
+	return found;
 }
 int listLength(node_t *head)
 {
 	int length = 0;
-	
+
 	node_t *temp = head;
-	
+
 	while (temp != NULL)
 	{
 		length++;
@@ -166,20 +145,19 @@ int listLength(node_t *head)
 }
 void printList(node_t *head)
 {
-	int  counter = 0;
+	int counter = 0;
 
 	if (head == NULL)
 	{
-		return;		// if list is empty, we exit.
+		return; // if list is empty, we exit.
 	}
-	
+
 	node_t *temp;
 	temp = head;
-	
-	
+
 	while (temp != NULL)
-	{	
-		printf("%d-> " , temp->value);
+	{
+		printf("%d-> ", temp->value);
 		if (counter == 10)
 		{
 			printf("\n");
@@ -190,6 +168,4 @@ void printList(node_t *head)
 		counter++;
 	}
 	printf("NULL\n\n\n");
-	
 }
-
